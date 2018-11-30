eab7548e748ce0ae846d5ad5208a2bcaa2918f98
Unknown <carlossas_97@hotmail.com>
2018-11-30 13:11:18 -0600

    AOS SCROLL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ScrollanimateService } from './services/scrollanimate.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    public animationScroll: ScrollanimateService
+  ) {
+   }
 }
